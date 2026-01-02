d2a3b9368b368e083e668a063c83a37b0b557603
mahi2808 <maheshkhot2808@gmail.com>
2026-01-02 17:57:24 +0530

    Add OPAMP example projects for PIC32CM5164LE00100

---
+++ PIC32CM/PIC32CM5164LE00100/05_OPAMP/OpAmp_ADC/OpAmp_ADC/opamp_From_Microchip/opamp_adc_gain/firmware/pic32cm_le00_curiosity_pro.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic32cm_le00_curiosity_pro configuration
+CND_ARTIFACT_DIR_pic32cm_le00_curiosity_pro=dist/pic32cm_le00_curiosity_pro/production
+CND_ARTIFACT_NAME_pic32cm_le00_curiosity_pro=pic32cm_le00_curiosity_pro.X.production.hex
+CND_ARTIFACT_PATH_pic32cm_le00_curiosity_pro=dist/pic32cm_le00_curiosity_pro/production/pic32cm_le00_curiosity_pro.X.production.hex
